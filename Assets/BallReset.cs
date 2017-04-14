cf5f223e9d18d9a2231597a2fb74e5bcd83a9a21
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-04-14 10:45:55 -0400

    ball reset when it hits the floor and grabbing and throwing functionality

---
+++ Assets/BallReset.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallReset : MonoBehaviour {
+
+    Vector3 initialPosition;
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        initialPosition = transform.position;
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            transform.position = initialPosition;
+            rigidBody.velocity = new Vector3(0, 0, 0);
+        }
+    }
+}
--- Assets/Scripts/ControllerInputManager.cs
+++ Assets/Scripts/ControllerInputManager.cs
@@ -14,6 +14,10 @@ public class ControllerInputManager : MonoBehaviour {
     public Vector3 teleportLocation;
     public GameObject player;
     public LayerMask layerMask;
+    public bool isLeftController;
+
+    //- grabbing and throwing
+    private float throwForce = 1.5f;
 
     void Start () {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
@@ -25,41 +29,85 @@ public class ControllerInputManager : MonoBehaviour {
         Teleport();
 	}
 
+    /* -------------------------------------------------------------------------------------------------------- //
+
+                                                    Teleporting
+    // -------------------------------------------------------------------------------------------------------- */
+
     void Teleport ()
     {
-        if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        if (isLeftController)
         {
-            laser.gameObject.SetActive(true);
-            teleportAimerObject.SetActive(true);
-
-            laser.SetPosition(0, gameObject.transform.position);
-            RaycastHit hit;
-            
-            if (Physics.Raycast(transform.position, transform.forward, out hit, 5, layerMask))
+            if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
             {
-                teleportLocation = hit.point;
-                laser.SetPosition(1, teleportLocation);
-                teleportAimerObject.transform.position = new Vector3(teleportLocation.x, teleportLocation.y + yNudgeAmount, teleportLocation.z);
-            } else
-            {
-                teleportLocation = new Vector3(transform.forward.x * 5 + transform.position.x, transform.forward.y * 5 + transform.position.y, transform.forward.z * 5 + transform.position.z);
-                RaycastHit groundRay;
+                laser.gameObject.SetActive(true);
+                teleportAimerObject.SetActive(true);
 
-                if (Physics.Raycast(teleportLocation, -Vector3.up, out groundRay, 7, layerMask))
+                laser.SetPosition(0, gameObject.transform.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(transform.position, transform.forward, out hit, 5, layerMask))
                 {
-                    teleportLocation = new Vector3(transform.forward.x * 5 + transform.position.x, groundRay.point.y, transform.forward.z * 5 + transform.position.z);
+                    teleportLocation = hit.point;
+                    laser.SetPosition(1, teleportLocation);
+                    teleportAimerObject.transform.position = new Vector3(teleportLocation.x, teleportLocation.y + yNudgeAmount, teleportLocation.z);
                 }
-                laser.SetPosition(1, transform.forward * 5 + transform.position);
-                teleportAimerObject.transform.position = teleportLocation + new Vector3(0, yNudgeAmount, 0);
+                else
+                {
+                    teleportLocation = new Vector3(transform.forward.x * 5 + transform.position.x, transform.forward.y * 5 + transform.position.y, transform.forward.z * 5 + transform.position.z);
+                    RaycastHit groundRay;
+
+                    if (Physics.Raycast(teleportLocation, -Vector3.up, out groundRay, 7, layerMask))
+                    {
+                        teleportLocation = new Vector3(transform.forward.x * 5 + transform.position.x, groundRay.point.y, transform.forward.z * 5 + transform.position.z);
+                    }
+                    laser.SetPosition(1, transform.forward * 5 + transform.position);
+                    teleportAimerObject.transform.position = teleportLocation + new Vector3(0, yNudgeAmount, 0);
+                }
+            }
+
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+            {
+                laser.gameObject.SetActive(false);
+                teleportAimerObject.SetActive(false);
+                player.transform.position = teleportLocation;
             }
         }
+    }
 
-/*        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+    /* -------------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+                                                                                    Grabbing and Throwing
+    // -------------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+    void GrabObject (Collider col)
+    {
+        col.transform.SetParent(gameObject.transform);
+        col.GetComponent<Rigidbody>().isKinematic = true;
+        device.TriggerHapticPulse(1000);
+    }
+
+    void ThrowObject (Collider col)
+    {
+        col.transform.SetParent(null);
+        Rigidbody rigidbody = col.GetComponent<Rigidbody>();
+        rigidbody.isKinematic = false;
+        rigidbody.velocity = device.velocity * throwForce;
+        rigidbody.angularVelocity = device.angularVelocity;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Throwable"))
         {
-            laser.gameObject.SetActive(false);
-            teleportAimerObject.SetActive(false);
-            player.transform.position = teleportLocation;
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                ThrowObject(other);
+            }
+            else if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                GrabObject(other);
+            }
         }
-*/
     }
 }
