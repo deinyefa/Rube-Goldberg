2a9224f683b6e3ba5d5d6eb40d5c574d7233207b
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-04-15 10:02:49 -0400

    main game mechanic - when all the stars have been hit, new scene loads

--- Assets/Scripts/BallReset.cs
+++ Assets/Scripts/BallReset.cs
@@ -6,6 +6,7 @@ public class BallReset : MonoBehaviour {
 
     Vector3 initialPosition;
     Rigidbody rigidBody;
+    public GameObject[] collectables;
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +16,7 @@ public class BallReset : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        LoadNewLevel();
 	}
 
     private void OnCollisionEnter(Collision other)
@@ -23,7 +24,41 @@ public class BallReset : MonoBehaviour {
         if (other.gameObject.CompareTag("Ground"))
         {
             transform.position = initialPosition;
-            rigidBody.velocity = new Vector3(0, 0, 0);
+            rigidBody.velocity = Vector3.zero;
+
+            for (int i = 0; i < collectables.Length; i++)
+            {
+                collectables[i].SetActive(true);
+            }
         }
+
+        foreach (GameObject collectable in collectables)
+        {
+            if (other.gameObject.CompareTag("Collectable"))
+            {
+                other.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void LoadNewLevel ()
+    {
+        if (AreStarsInActive())
+        {
+            Debug.Log("loading Scene 1");
+            SteamVR_LoadLevel.Begin("Scene1");
+        }
+    }
+
+    bool AreStarsInActive ()
+    {
+        foreach (GameObject collectable in collectables)
+        {
+            if (collectable.gameObject.activeInHierarchy)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
---
+++ Assets/Scripts/OVRContollerInputManager.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OVRContollerInputManager : MonoBehaviour {
+    private SteamVR_TrackedObject trackedObj;
+    public SteamVR_Controller.Device device;
+
+    [Header("Teleporting")]
+    //- to teleport
+    public bool isLeftController;
+    private LineRenderer laser;
+    private float yNudgeAmount = 0.1f;
+    public GameObject teleportAimerObject;
+    public Vector3 teleportLocation;
+    public GameObject player;
+    public LayerMask layerMask;
+
+    [Header("Grabbing and Throwing")]
+    //- grabbing and throwing
+    private OVRInput.Controller thisController;
+    private float throwForce = 1.5f;
+
+    [Header("Object Menu")]
+    //- object menu
+    public bool isRightController;
+    private float swipeSum;
+    private float touchLast;
+    private float touchCurrent;
+    private float distance;
+    private bool hasSwipedLeft;
+    private bool hasSwipedRight;
+    public ObjectMenuManager objectMenuManager;
+    private bool menuIsSwipeable;
+    private float menuStickX;
+
+
+
+    //------------------------------------------------------------------------------------------------------
+
+    void Start()
+    {
+        laser = GetComponentInChildren<LineRenderer>();
+
+        if (isLeftController)
+        {
+            thisController = OVRInput.Controller.LTouch;
+        } else
+        {
+            thisController = OVRInput.Controller.RTouch;
+        }
+    }
+
+    void Update()
+    {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+        Teleport();
+        ObjectMenu();
+    }
+
+    /* -------------------------------------------------------------------------------------------------------- //
+
+                                                    Teleporting
+    // -------------------------------------------------------------------------------------------------------- */
+
+    void Teleport()
+    {
+        if (isLeftController)
+        {
+            if (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
+            {
+                laser.gameObject.SetActive(true);
+                teleportAimerObject.SetActive(true);
+
+                laser.SetPosition(0, gameObject.transform.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(transform.position, transform.forward, out hit, 5, layerMask))
+                {
+                    teleportLocation = hit.point;
+                    laser.SetPosition(1, teleportLocation);
+                    teleportAimerObject.transform.position = new Vector3(teleportLocation.x, teleportLocation.y + yNudgeAmount, teleportLocation.z);
+                }
+                else
+                {
+                    teleportLocation = new Vector3(transform.forward.x * 5 + transform.position.x, transform.forward.y * 5 + transform.position.y, transform.forward.z * 5 + transform.position.z);
+                    RaycastHit groundRay;
+
+                    if (Physics.Raycast(teleportLocation, -Vector3.up, out groundRay, 7, layerMask))
+                    {
+                        teleportLocation = new Vector3(transform.forward.x * 5 + transform.position.x, groundRay.point.y, transform.forward.z * 5 + transform.position.z);
+                    }
+                    laser.SetPosition(1, transform.forward * 5 + transform.position);
+                    teleportAimerObject.transform.position = teleportLocation + new Vector3(0, yNudgeAmount, 0);
+                }
+            }
+
+            if (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
+            {
+                laser.gameObject.SetActive(false);
+                teleportAimerObject.SetActive(false);
+                player.transform.position = teleportLocation;
+            }
+        }
+    }
+
+    /* -------------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+                                                                                    Grabbing and Throwing
+    // -------------------------------------------------------------------------------------------------------------------------------------------------------- */
+    
+
+    void GrabObject(Collider col)
+    {
+        col.transform.SetParent(gameObject.transform);
+        col.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    void ThrowObject(Collider col)
+    {
+        col.transform.SetParent(null);
+        Rigidbody rigidbody = col.GetComponent<Rigidbody>();
+        rigidbody.isKinematic = false;
+        rigidbody.velocity = OVRInput.GetLocalControllerVelocity(thisController) * throwForce;
+        rigidbody.angularVelocity = OVRInput.GetLocalControllerAngularVelocity(thisController);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Throwable"))
+        {
+            if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, thisController) < 0.1f)
+            {
+                ThrowObject(other);
+            }
+            else if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, thisController) > 0.1f)
+            {
+                GrabObject(other);
+            }
+        }
+
+        if (other.gameObject.CompareTag("Structure"))
+        {
+            if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, thisController) < 0.1f)
+            {
+                other.transform.SetParent(null);
+                Rigidbody rigidbody = other.GetComponent<Rigidbody>();
+                rigidbody.isKinematic = false;
+            }
+            else if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, thisController) > 0.1f)
+            {
+                GrabObject(other);
+            }
+        }
+    }
+
+    /* -------------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+                                                                                   Object Menu
+   // -------------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+    void ObjectMenu()
+    {
+        if (isLeftController)
+        {
+            menuStickX = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, thisController).x;
+            if (menuStickX < 0.45f && menuStickX > -0.45f)
+            {
+                menuIsSwipeable = true;
+            }
+            if (menuIsSwipeable)
+            {
+                if (menuStickX >= 0.45f)
+                {
+                    objectMenuManager.MenuRight();
+                    menuIsSwipeable = false;
+                }
+                else if (menuStickX <= -0.45f)
+                {
+                    objectMenuManager.MenuLeft();
+                    menuIsSwipeable = false;
+                }
+            }
+        }
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, thisController))
+        {
+            objectMenuManager.SpwanCurrentObject();
+        }
+    }
+
+    void SwipeRight()
+    {
+        objectMenuManager.MenuRight();
+    }
+
+    void SwipeLeft()
+    {
+        objectMenuManager.MenuLeft();
+    }
+
+    void SpawnObject()
+    {
+        objectMenuManager.SpwanCurrentObject();
+    }
+}
