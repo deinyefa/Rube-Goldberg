fcb728631f85393f53f4dcbacdb55c6f3446ea1e
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-05-01 23:44:52 -0400

    tutorial script

---
+++ Assets/Scripts/HintVariables.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintVariables : MonoBehaviour {
+    
+    private bool hasTeleported;
+    private bool ballHasBeenLifted;
+    private bool hasCompletedLeftControllerTutorial;
+    private bool hasCompletedRightControllerTutorial;
+
+    public bool HasTeleport
+    {
+        get
+        {
+            return hasTeleported;
+        }
+        set
+        {
+            hasTeleported = true;
+        }
+    }
+
+    public bool BallHasBeenLifted
+    {
+        get
+        {
+            return ballHasBeenLifted;
+        }
+        set
+        {
+            ballHasBeenLifted = true;
+        }
+    }
+
+    public bool LeftControllerTutorialCompleted
+    {
+        get
+        {
+            return hasCompletedLeftControllerTutorial;
+        } 
+        set
+        {
+            hasCompletedLeftControllerTutorial = true;
+        }
+    }
+
+    public bool RightControllerTutorialConpleted
+    {
+        get
+        {
+            return hasCompletedRightControllerTutorial;
+        }
+        set
+        {
+            hasCompletedRightControllerTutorial = true; 
+        }
+    }
+}
--- Assets/Scripts/HintsManager.cs
+++ Assets/Scripts/HintsManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using Valve.VR.InteractionSystem;
 
 public class HintsManager : MonoBehaviour {
@@ -9,26 +10,31 @@ public class HintsManager : MonoBehaviour {
     private SteamVR_TrackedObject trackedObj;
     private SteamVR_Controller.Device device;
 
+    private bool hasTeleported = false;
+    private bool ballHasBeenLifted = false;
+    private bool hasCompletedLeftControllerTutorial = false;
+    private bool hasCompletedRightControllerTutorial = false;
+
     public bool isLeftController;
     public bool isRightController;
-
-    private ControllerButtonHints contollerButtonHints;
+    public Text tutorialText;
 
     void Awake ()
     {
-        trackedObj = GetComponent<SteamVR_TrackedObject>();
-        contollerButtonHints = GetComponent<ControllerButtonHints>();
+        trackedObj = GetComponentInParent<SteamVR_TrackedObject>();
+        tutorialText = GetComponentInChildren<Text>();
 	}
 
     void Start ()
     {
-        
+
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
         device = SteamVR_Controller.Input((int)trackedObj.index);
+        DisplayHints();
     }
 
     void DisplayHints ()
@@ -38,24 +44,52 @@ public class HintsManager : MonoBehaviour {
             if (isLeftController)
             {
                 //- loads the teleportation hint when user touches the touchpad
-                if (device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
+                if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
                 {
-                    contollerButtonHints.textHintPrefab = Resources.Load("teleportation_hint") as GameObject;
+                    tutorialText.text = "To teleport, hold and\nrelease the touchpad";
                 }
-                //- removes the hint when finger leaves the touchpad
-                else if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad))
-                {
-                    contollerButtonHints.textHintPrefab = null;
-                }
-                //- if user has teleported, show the hint that leades player to the objects menu
                 if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
                 {
-                    contollerButtonHints.textHintPrefab = Resources.Load("go_to_menu_hint") as GameObject;
+                    tutorialText.text = "Touch the ball with either\ncontroller and hold the trigger\nto lift it";
+                    hasTeleported = true;
+                }
+                if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+                {
+                    ballHasBeenLifted = true;
+                }
+                if (ballHasBeenLifted && hasTeleported)
+                {
+                    tutorialText.text = "Press the trigger on the right\ncontroller to continue";
+                    hasCompletedLeftControllerTutorial = true;
+                    Debug.Log(hasCompletedLeftControllerTutorial);
                 }
             }
             if (isRightController)
             {
+                
+                if (ballHasBeenLifted && hasTeleported)
+                {
+                    tutorialText.text = "With the right controller,\nswipe left or right\nto choose a tool";
+                }
+                if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+                {
+                    tutorialText.text = "To choose a tool,\ntouch the touchpad and\npress the trigger.\nMove the tool like\nyou would the ball";
 
+                }
+                //- can only complete right controller tutorial if player has completed left controller tutorial
+                if (hasCompletedLeftControllerTutorial)
+                {
+                    hasCompletedRightControllerTutorial = true;
+                    if (hasCompletedLeftControllerTutorial && hasCompletedRightControllerTutorial)
+                    {
+                        Debug.Log(hasCompletedLeftControllerTutorial);
+
+                        if (isLeftController)
+                            tutorialText.gameObject.SetActive(false);
+                        if (isRightController)
+                            tutorialText.gameObject.SetActive(false);
+                    }
+                }
             }
         }
     }
